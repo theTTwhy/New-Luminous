0605022778a81ed361b418937e475378a1ba541e
theTTwhy <vanderpoelnoud@gmail.com>
2019-02-15 16:48:56 +0100

    Basic three js project
    Push 1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>New-Luminous</title>
+    <meta name="description" content="New-Luminous">
+    <meta name="author" content="Noudvanderpoel.nl">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/101/three.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
